c287fdeaceacaf3a884ae3ad2d6d7da5857082bb
Angelina Ivanova <angelina@gmail.com>
2025-11-20 23:19:36 +0200

    Updated UI

--- AnimalShelter/AnimalShelter/Form1.Designer.cs
+++ AnimalShelter/AnimalShelter/Form1.Designer.cs
@@ -42,29 +42,32 @@
             this.btnUpdate = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.colName = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.colID = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.colAge = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.colSpecies = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.colState = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.label5 = new System.Windows.Forms.Label();
             this.btnAdopt = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.colState = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colSpecies = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colAge = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel2 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.numericAge)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(110, 21);
+            this.txtName.Location = new System.Drawing.Point(109, 15);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(152, 22);
+            this.txtName.Size = new System.Drawing.Size(126, 22);
             this.txtName.TabIndex = 0;
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(24, 24);
+            this.label1.Location = new System.Drawing.Point(21, 18);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 22);
+            this.label1.Size = new System.Drawing.Size(57, 16);
             this.label1.TabIndex = 1;
             this.label1.Text = "Name";
             // 
@@ -77,15 +80,15 @@
             "Rabbit",
             "Parrot",
             "Hamster"});
-            this.comboSpecies.Location = new System.Drawing.Point(110, 108);
+            this.comboSpecies.Location = new System.Drawing.Point(109, 103);
             this.comboSpecies.Name = "comboSpecies";
-            this.comboSpecies.Size = new System.Drawing.Size(143, 24);
+            this.comboSpecies.Size = new System.Drawing.Size(126, 24);
             this.comboSpecies.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(24, 116);
+            this.label2.Location = new System.Drawing.Point(21, 106);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(57, 16);
             this.label2.TabIndex = 3;
@@ -93,15 +96,15 @@
             // 
             // numericAge
             // 
-            this.numericAge.Location = new System.Drawing.Point(110, 66);
+            this.numericAge.Location = new System.Drawing.Point(109, 59);
             this.numericAge.Name = "numericAge";
-            this.numericAge.Size = new System.Drawing.Size(147, 22);
+            this.numericAge.Size = new System.Drawing.Size(126, 22);
             this.numericAge.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(29, 60);
+            this.label3.Location = new System.Drawing.Point(21, 62);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(32, 16);
             this.label3.TabIndex = 5;
@@ -113,7 +116,7 @@
             this.comboState.Items.AddRange(new object[] {
             "Adopted",
             "Not adopted"});
-            this.comboState.Location = new System.Drawing.Point(117, 155);
+            this.comboState.Location = new System.Drawing.Point(109, 149);
             this.comboState.Name = "comboState";
             this.comboState.Size = new System.Drawing.Size(126, 24);
             this.comboState.TabIndex = 6;
@@ -121,7 +124,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(25, 151);
+            this.label4.Location = new System.Drawing.Point(21, 150);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(38, 16);
             this.label4.TabIndex = 7;
@@ -129,7 +132,7 @@
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(381, 145);
+            this.btnExit.Location = new System.Drawing.Point(9, 78);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(134, 29);
             this.btnExit.TabIndex = 8;
@@ -138,34 +141,34 @@
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(392, 77);
+            this.btnAdd.Location = new System.Drawing.Point(9, 10);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(126, 22);
+            this.btnAdd.Size = new System.Drawing.Size(134, 29);
             this.btnAdd.TabIndex = 9;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
             // 
             // btnClear
             // 
-            this.btnClear.Location = new System.Drawing.Point(392, 105);
+            this.btnClear.Location = new System.Drawing.Point(9, 44);
             this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(123, 28);
+            this.btnClear.Size = new System.Drawing.Size(134, 29);
             this.btnClear.TabIndex = 10;
             this.btnClear.Text = "Clear";
             this.btnClear.UseVisualStyleBackColor = true;
             // 
             // btnUpdate
             // 
-            this.btnUpdate.Location = new System.Drawing.Point(590, 127);
+            this.btnUpdate.Location = new System.Drawing.Point(149, 44);
             this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Size = new System.Drawing.Size(128, 28);
+            this.btnUpdate.Size = new System.Drawing.Size(127, 28);
             this.btnUpdate.TabIndex = 11;
             this.btnUpdate.Text = "Update";
             this.btnUpdate.UseVisualStyleBackColor = true;
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(590, 162);
+            this.btnDelete.Location = new System.Drawing.Point(149, 78);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(127, 28);
             this.btnDelete.TabIndex = 12;
@@ -174,6 +177,7 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.colName,
@@ -181,93 +185,97 @@
             this.colAge,
             this.colSpecies,
             this.colState});
-            this.dataGridView1.Location = new System.Drawing.Point(5, 215);
+            this.dataGridView1.Location = new System.Drawing.Point(32, 253);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(738, 167);
             this.dataGridView1.TabIndex = 13;
             // 
-            // colName
+            // btnAdopt
             // 
-            this.colName.HeaderText = "Name";
-            this.colName.MinimumWidth = 6;
-            this.colName.Name = "colName";
-            this.colName.Width = 125;
+            this.btnAdopt.Location = new System.Drawing.Point(149, 10);
+            this.btnAdopt.Name = "btnAdopt";
+            this.btnAdopt.Size = new System.Drawing.Size(127, 28);
+            this.btnAdopt.TabIndex = 15;
+            this.btnAdopt.Text = "Adopt";
+            this.btnAdopt.UseVisualStyleBackColor = true;
             // 
-            // colID
+            // panel1
             // 
-            this.colID.HeaderText = "ID";
-            this.colID.MinimumWidth = 6;
-            this.colID.Name = "colID";
-            this.colID.Width = 125;
-            // 
-            // colAge
-            // 
-            this.colAge.HeaderText = "Age";
-            this.colAge.MinimumWidth = 6;
-            this.colAge.Name = "colAge";
-            this.colAge.Width = 125;
-            // 
-            // colSpecies
-            // 
-            this.colSpecies.HeaderText = "Species";
-            this.colSpecies.MinimumWidth = 6;
-            this.colSpecies.Name = "colSpecies";
-            this.colSpecies.Width = 125;
+            this.panel1.Controls.Add(this.btnAdopt);
+            this.panel1.Controls.Add(this.btnDelete);
+            this.panel1.Controls.Add(this.btnUpdate);
+            this.panel1.Controls.Add(this.btnClear);
+            this.panel1.Controls.Add(this.btnAdd);
+            this.panel1.Controls.Add(this.btnExit);
+            this.panel1.Location = new System.Drawing.Point(484, 69);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(286, 118);
+            this.panel1.TabIndex = 16;
             // 
             // colState
             // 
             this.colState.HeaderText = "State";
             this.colState.MinimumWidth = 6;
             this.colState.Name = "colState";
-            this.colState.Width = 125;
             // 
-            // label5
+            // colSpecies
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(378, 9);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(93, 16);
-            this.label5.TabIndex = 14;
-            this.label5.Text = "Animal Shelter";
+            this.colSpecies.HeaderText = "Species";
+            this.colSpecies.MinimumWidth = 6;
+            this.colSpecies.Name = "colSpecies";
             // 
-            // btnAdopt
+            // colAge
             // 
-            this.btnAdopt.Location = new System.Drawing.Point(592, 67);
-            this.btnAdopt.Name = "btnAdopt";
-            this.btnAdopt.Size = new System.Drawing.Size(125, 30);
-            this.btnAdopt.TabIndex = 15;
-            this.btnAdopt.Text = "Adopt";
-            this.btnAdopt.UseVisualStyleBackColor = true;
+            this.colAge.HeaderText = "Age";
+            this.colAge.MinimumWidth = 6;
+            this.colAge.Name = "colAge";
+            // 
+            // colID
+            // 
+            this.colID.HeaderText = "ID";
+            this.colID.MinimumWidth = 6;
+            this.colID.Name = "colID";
+            // 
+            // colName
+            // 
+            this.colName.HeaderText = "Name";
+            this.colName.MinimumWidth = 6;
+            this.colName.Name = "colName";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.comboState);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.numericAge);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.comboSpecies);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.txtName);
+            this.panel2.Location = new System.Drawing.Point(32, 21);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(273, 190);
+            this.panel2.TabIndex = 17;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnAdopt);
-            this.Controls.Add(this.label5);
+            this.ClientSize = new System.Drawing.Size(805, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.btnDelete);
-            this.Controls.Add(this.btnUpdate);
-            this.Controls.Add(this.btnClear);
-            this.Controls.Add(this.btnAdd);
-            this.Controls.Add(this.btnExit);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.comboState);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.numericAge);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.comboSpecies);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtName);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Animal Shelter";
             ((System.ComponentModel.ISupportInitialize)(this.numericAge)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -287,13 +295,14 @@
         private System.Windows.Forms.Button btnUpdate;
         private System.Windows.Forms.Button btnDelete;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnAdopt;
+        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.DataGridViewTextBoxColumn colName;
         private System.Windows.Forms.DataGridViewTextBoxColumn colID;
         private System.Windows.Forms.DataGridViewTextBoxColumn colAge;
         private System.Windows.Forms.DataGridViewTextBoxColumn colSpecies;
         private System.Windows.Forms.DataGridViewTextBoxColumn colState;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Button btnAdopt;
+        private System.Windows.Forms.Panel panel2;
     }
 }
 
